2ecba24c19fcb6322913c5a6a72fc9c244978a36
HectorzBarz <hrodrigue@inscamidemar.cat>
2026-01-27 17:46:54 +0100

    feat: Create Nif Class

---
+++ src/toreno/Model/Nif.java
@@ -0,0 +1,42 @@
+package toreno.Model;
+
+public class Nif {
+	
+	/* -- Attributes -- */
+	private int dni;
+	private String lletra;
+	
+	public int getDni() {
+		return dni;
+	}
+
+	
+	/* -- Getters / Setters -- */
+	public void setDni(int dni) {
+		if (String.valueOf(dni).length() != 8) {
+	        throw new IllegalArgumentException("¡El DNI debe tener 8 dígitos!");
+	    }
+	    this.dni = dni;
+	}
+
+	public String getLletra() {
+		return lletra;
+	}
+
+	public void setLletra(String lletra) {
+		if(!lletra.isBlank() || !lletra.isEmpty() || lletra != null) {
+			this.lletra = lletra;			
+		}
+	}
+
+	/* -- Constructor -- */
+	public Nif(int dni, String lletra) {
+		setDni(dni);
+		setLletra(lletra);
+	}
+	
+	@Override
+	public String toString() {
+		return getDni() + getLletra();
+	}
+}
