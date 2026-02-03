9d1f92bf11291de9bfe473f67b2c72156f5f95ca
HectorzBarz <hrodrigue@inscamidemar.cat>
2026-02-03 17:35:06 +0100

    fix: add Torneig classs because i forgot to push o feature/classes

---
+++ src/toreno/Model/Torneig.java
@@ -0,0 +1,62 @@
+package toreno.Model;
+
+public class Torneig {
+
+    /* -- Attributes -- */
+    private Data data;
+    private int partitsJugats;
+    private Partit[] partits;
+    private Jugador guanyador;
+
+    /* -- Getters / Setters -- */
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public int getPartitsJugats() {
+        return partitsJugats;
+    }
+
+    public void setPartitsJugats(int partitsJugats) {
+        this.partitsJugats = partitsJugats;
+    }
+
+    public Partit[] getPartits() {
+        return partits;
+    }
+
+    public void setPartits(Partit[] partits) {
+        this.partits = partits;
+    }
+
+    public Jugador getGuanyador() {
+        return guanyador;
+    }
+
+    public void setGuanyador(Jugador guanyador) {
+        this.guanyador = guanyador;
+    }
+
+    /* -- Constructor -- */
+    public Torneig(Data data, int partitsJugats, Partit[] partits, Jugador guanyador) {
+        setData(data);
+        setPartitsJugats(partitsJugats);
+        setPartits(partits);
+        setGuanyador(guanyador);
+    }
+
+    /* -- toString -- */
+    @Override
+    public String toString() {
+        return "Torneig{" +
+                "data=" + data +
+                ", partitsJugats=" + partitsJugats +
+                ", partits=" + java.util.Arrays.toString(partits) +
+                ", guanyador=" + guanyador +
+                '}';
+    }
+}
